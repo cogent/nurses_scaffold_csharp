866758c622dc915143b69c94b78bb5a2a6c9be8d
Lonnie King <lonnie.king@me.com>
2020-05-01 09:47:58 +1000

    created project/solution files

---
+++ Nurses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nurses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
