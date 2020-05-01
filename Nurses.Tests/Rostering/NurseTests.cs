401184a90fbfb170cd3c2bdf5cd4878f945c4cfd
Lonnie King <lonnie.king@me.com>
2020-05-01 11:29:40 +1000

    add equality for nurses

--- Nurses.Tests/Rostering/NurseTests.cs
+++ Nurses.Tests/Rostering/NurseTests.cs
@@ -9,6 +9,8 @@ namespace Nurses.Tests.Rostering
         private static string name = "Natasha";
         private static string uid = "19ab7c";
 
+        #region Attributes
+
         [Fact]
         public void Nurse_Uid_SetGet()
         {
@@ -26,5 +28,59 @@ namespace Nurses.Tests.Rostering
 
             Assert.Equal(nurse.name, name);
         }
+        
+        #endregion
+
+        #region Equality
+
+        [Fact]
+        public void Nurse_Equality_WhenOtherHasSameNameAndUid_IsEqual()
+        {
+            Assert.Equal(
+                new Nurse { name = name, uid = uid },
+                new Nurse { name = name, uid = uid }
+            );
+        }
+
+        [Fact]
+        public void Nurse_Equality_WhenOtherHasSameObjectReference_IsEqual()
+        {
+            var nurseA = new Nurse { name = name, uid = uid };
+            var nurseB = nurseA;
+
+            Assert.Equal(nurseA, nurseB);
+        }
+
+        [Fact]
+        public void Nurse_Equality_WhenOtherHasSameUidDifferentName_IsNotEqual()
+        {
+            var nurse = new Nurse { name = "Natasha Romanoff", uid = uid };
+            var sameNurse = new Nurse { name = "Clint Barton", uid = uid };
+
+            Assert.NotEqual(nurse, sameNurse);
+        }
+
+        [Fact]
+        public void Nurse_Equality_WhenOtherHasSameNameDifferentUid_IsNotEqual()
+        {
+            var nurse = new Nurse { name = name, uid = "19ab7c45" };
+            var sameNurse = new Nurse { name = name, uid = "20ef8g56" };
+
+            Assert.NotEqual(nurse, sameNurse);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("19ab7c")]
+        [InlineData("Natasha")]
+        [InlineData(999)]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void Nurse_Equality_WhenOtherIsNotNurse_IsNotEqual(object testInput)
+        {
+            Assert.NotEqual(new Nurse { name = name, uid = uid }, testInput);
+        }
+
+        #endregion
     }
 }
